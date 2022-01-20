b37732d46013817248272ef9d1fe3568a932cd75
user <user@Dell.bbrouter>
2022-01-20 18:14:55 +0530

    done

--- onlinetripmanagement/src/main/java/com/cg/webapp/OnlinetripmanagementApplication.java
+++ onlinetripmanagement/src/main/java/com/cg/webapp/OnlinetripmanagementApplication.java
@@ -12,7 +12,7 @@ public class OnlinetripmanagementApplication {
 
 	public static void main(String[] args) {
 
-	  
+	  //ksfjbhjvkdngj
 		
 		log.info("Online Trip Management Application Started");
 
